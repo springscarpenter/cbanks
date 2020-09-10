204495f59912bdd8722a6a4996d063603a725fb3
Springs Carpenter <70685791+springscarpenter@users.noreply.github.com>
2020-09-10 22:05:53 +0300

    Edit Disclaimer

--- client/src/components/utils/Disclaimer.js
+++ client/src/components/utils/Disclaimer.js
@@ -4,17 +4,20 @@ const Disclaimer = () => {
   return (
     <div className='backdrop' id='disclaimer'>
       <div className='disclaimer-msg'>
+        <h3>Risk Disclosure</h3>
         <p>
-          Disclaimer: Information provided on this site may contain inaccuracies
-          and/or discrepancies with real-time data. Use of content is solely at
-          your own risk and discretion.
+          Information provided on this site may contain inaccuracies and/or
+          discrepancies with real-time data. Use of content is solely at your
+          own risk and discretion.
         </p>
-        <button
-          className='btn disclaimer-btn'
-          onClick={() => document.querySelector('#disclaimer').remove()}
-        >
-          I Understand
-        </button>
+        <div className='disclaimer-btn-box'>
+          <button
+            className='btn'
+            onClick={() => document.querySelector('#disclaimer').remove()}
+          >
+            I Understand
+          </button>
+        </div>
       </div>
     </div>
   );

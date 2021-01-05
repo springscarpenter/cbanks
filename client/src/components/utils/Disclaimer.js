2abc35581beffad35ed9ab35a8bb918fdec409e5
Springs Carpenter <70685791+springscarpenter@users.noreply.github.com>
2021-01-05 07:10:24 +0300

    Update style

--- client/src/components/utils/Disclaimer.js
+++ client/src/components/utils/Disclaimer.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Disclaimer = () => {
+  useEffect(() => {
+    document.body.style.position = 'fixed';
+    document.body.style.width = '100%';
+    document.body.style.top = `-${window.scrollY}px`;
+  }, []);
+
   return (
     <div className='backdrop' id='disclaimer'>
       <div className='disclaimer-msg'>
@@ -13,7 +19,14 @@ const Disclaimer = () => {
         <div className='disclaimer-btn-box'>
           <button
             className='btn'
-            onClick={() => document.querySelector('#disclaimer').remove()}
+            onClick={() => {
+              document.querySelector('#disclaimer').remove();
+              const scrollY = document.body.style.top;
+              document.body.style.position = '';
+              document.body.style.width = '';
+              document.body.style.top = '';
+              window.scrollTo(0, parseInt(scrollY || '0') * -1);
+            }}
           >
             I Understand
           </button>
